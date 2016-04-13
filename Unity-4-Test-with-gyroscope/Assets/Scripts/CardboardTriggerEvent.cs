390da22c50023b95c2bd62a29deeb56746411187
adam <adam.alsegard@gmail.com>
2016-04-13 16:15:31 +0200

    Trigger event added

--- Unity-4-Test-with-gyroscope/Assets/Scripts/CardboardTriggerEvent.cs
+++ Unity-4-Test-with-gyroscope/Assets/Scripts/CardboardTriggerEvent.cs
@@ -1,25 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CardboardTriggerEvent : MonoBehaviour {
 
 	public Cardboard myCardboard;
+	private GameObject projectile;
+	private List<GameObject> myBowlingBalls = new List<GameObject>();
 
 	void Start() {
 		// Disable screen dimming:
 		Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
+		projectile = GameObject.FindGameObjectWithTag("projectile");
 	} 
 
 	void Update () {
 		//True for only one frame
 		if(myCardboard.Triggered) {
 			Debug.Log("Button was clicked");
-			createNewCube ();
-
+			 sendNewBall();
+		}
+		foreach (GameObject ball in myBowlingBalls) {
+			if (ball.transform.position.magnitude < 60) {
+				ball.transform.position += ball.transform.forward * 5;
+			} 
+			else {
+				myBowlingBalls.Remove (ball);
+			}
 		}
 	}
-	void createNewCube(){
-		transform.position += Vector3.right*5; 
-	
+
+	void sendNewBall(){
+		GameObject player = GameObject.FindGameObjectWithTag("player");
+		GameObject newBall = Instantiate (projectile) as GameObject;
+		newBall.transform.position = player.transform.position;
+		newBall.transform.rotation = player.transform.rotation;
+		myBowlingBalls.Add (newBall);
+
 	}
 }
+
